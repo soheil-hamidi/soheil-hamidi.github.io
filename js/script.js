7eff190528050714d4892b1f7735659ab9184e28
Soheil Hamidi <soheilhamidi@me.com>
2017-07-09 23:18:06 -0400

    Template done!

--- js/script.js
+++ js/script.js
@@ -7,65 +7,109 @@ $(document).ready(function(ev) {
   })
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+$('.ball').click(function() {
+  $('#' + this.id).animateCss('bounce');
+});
+
+$('.places').click(function() {
+  var element_id = '#' + this.id;
+  $('.places').removeClass('places_active');
+  $(element_id).addClass('places_active');
+  if (element_id === '#hackathons') {
+    $('.controls').addClass('hide');
+    $('#hackathon-controls').removeClass('hide');
+    $('#hackathon-controls').animateCss('fadeIn');
+  } else if (element_id === '#work-experiences') {
+    $('.controls').addClass('hide');
+    $('#work-controls').removeClass('hide');
+    $('#work-controls').animateCss('fadeIn');
+  } else if (element_id === '#projects') {
+    $('.controls').addClass('hide');
+    $('#projects-controls').removeClass('hide');
+    $('#projects-controls').animateCss('fadeIn');
+  }
+});
 
 $('#third').click(async function() {
-  $('#avatar').addClass('hide');
-  $('#about').addClass('hide');
-  $('#custom_carousel').addClass('hide');
+  $('#avatar-col,#about,#experiences,#custom_carousel').addClass('hide');
+
   $('#first').addClass('animated slideOutUp');
   $('#bio-text').addClass('animated fadeOut');
   $('.fa-angle-down').removeClass('infinite fadeInDown');
   $('.fa-angle-down').addClass('fadeOutDown');
+
   await sleep(1000);
+
   $('html,body').animate({
       scrollTop: $('#custom_navbar').offset().top
     },
     1000);
+
   await sleep(1000);
+
   $('#first').removeClass('animated slideOutUp');
   $('#bio-text').removeClass('animated fadeOut');
-  $('.fa-angle-down').removeClass('fadeOutDown');
   $('.fa-angle-down').addClass('infinite fadeInDown');
-  $('#avatar').removeClass('hide');
-  $('#about').removeClass('hide');
-  $('#custom_carousel').removeClass('hide');
-  $('#about-col').addClass('animated fadeIn');
+  $('.fa-angle-down').removeClass('fadeOutDown');
+
+  $('#avatar-col,#about,#experiences,#custom_carousel').removeClass('hide');
+
   $('#avatar-col').addClass('animated flipInY');
+  $('#about').addClass('animated fadeIn');
+  $('#experiences').addClass('animated flipInX');
   $('#custom_carousel').addClass('animated fadeIn');
+
   await sleep(1000);
+
   $('#avatar-col').removeClass('animated flipInY');
-  $('#about-col').removeClass('animated fadeIn');
+  $('#about').removeClass('animated fadeIn');
+  $('#experiences').removeClass('animated flipInX');
+  $('#custom_carousel').removeClass('animated fadeIn');
 });
 
 $('#about').click(async function() {
-  $('#custom_carousel').addClass('animated fadeOut');
+  $('#first,#bio-text,.fa-angle-down').addClass('hide');
+
   $('#avatar-col').addClass('animated flipOutY');
-  $('#about-col').addClass('animated fadeOut');
+  $('#about').addClass('animated fadeOut');
+  $('#experiences').addClass('animated flipOutX');
+  $('#custom_carousel').addClass('animated fadeOut');
+
   await sleep(1000);
-  $('#custom_carousel').removeClass('animated fadeOut');
-  $('#avatar-col').removeClass('animated flipOutY');
-  $('#about-col').removeClass('animated fadeOut');
-  $('#avatar').addClass('hide');
-  $('#about').addClass('hide');
-  $('#custom_carousel').addClass('hide');
+
   $('html,body').animate({
       scrollTop: $('#first').offset().top
     },
     1000);
+
+  await sleep(1000);
+
+  $('#avatar-col').removeClass('animated flipOutY');
+  $('#about').removeClass('animated fadeOut');
+  $('#experiences').removeClass('animated flipOutX');
+  $('#custom_carousel').removeClass('animated fadeOut');
+
+  $('#first,#bio-text,.fa-angle-down').removeClass('hide');
+
+  $('#first').addClass('animated slideInDown');
+  $('#bio-text').addClass('animated fadeIn');
+  $('.fa-angle-down').addClass('infinite fadeInDown');
+
+  await sleep(1000);
+
+  $('#first').removeClass('animated slideInDown');
+  $('#bio-text').removeClass('animated fadeIn');
 });
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 $.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-
-$('.ball').click(function() {
-  $('#' + this.id).animateCss('bounce');
+  animateCss: function(animationName) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+    });
+  }
 });
