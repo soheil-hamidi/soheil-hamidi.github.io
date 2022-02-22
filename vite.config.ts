733101c439cbb4d8e7ab40f47ff3d713b778c1ff
Soheil Hamidi <soheil-hamidi@users.noreply.github.com>
2022-02-21 23:20:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: './',
   plugins: [vue(), eslintPlugin()],
   server: {
     host: true,
